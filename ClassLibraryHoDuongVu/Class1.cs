6d1e9485f35c62fa138cec8c5b980df84d59f7dd
Eatmsts <deseatmsts@gmail.com>
2021-05-28 16:51:24 +0700

    first commit

---
+++ ClassLibraryHoDuongVu/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryHoDuongVu
+{
+    public class Class1
+    {
+        public String HowToGrade(float exam, float course)
+        {
+            if ((exam < 0.0) || (exam > 100.0) || (course < 0.0) || (course > 100.00))
+                return "";
+            else
+            {
+                if ((exam < 50.0) || (course < 50.0))
+                    return "Fail";
+                else if (exam < 60)
+                    return "C";
+                else
+                {
+                    float averange = (exam + course) / 2.0f;
+                    if (averange >= 70.0)
+                        return "B";
+                    else
+                        return "A";
+                }
+            }
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibraryHoDuongVu;
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClassLibraryHoDuongVu.Class1 o = new Class1();
+            String kq = "";
+            String ex = o.HowToGrade(-1, 0);
+            Assert.AreEqual(kq, ex);
+            
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            ClassLibraryHoDuongVu.Class1 o = new Class1();
+            String kq = "Fail";
+            String ex = o.HowToGrade(49, 49);
+            Assert.AreEqual(kq, ex);
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            ClassLibraryHoDuongVu.Class1 o = new Class1();
+            String kq = "C";
+            String ex = o.HowToGrade(59, 59);
+            Assert.AreEqual(kq, ex);
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            ClassLibraryHoDuongVu.Class1 o = new Class1();
+            String kq = "B";
+            String ex = o.HowToGrade(70, 70);
+            Assert.AreEqual(kq, ex);
+
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            ClassLibraryHoDuongVu.Class1 o = new Class1();
+            String kq = "A";
+            String ex = o.HowToGrade(69, 69);
+            Assert.AreEqual(kq, ex);
+
+        }
+    }
+}
